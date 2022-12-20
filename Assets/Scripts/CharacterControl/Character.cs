ef2ddef25e0d8f6f15a29f4639b115fad7c67b68
Kyler Li <46853114+loukylor@users.noreply.github.com>
2022-12-19 17:21:39 -0800

    Transitions (#11)
    * Transitions
    
    * doesn't compile
    
    error CS0246: The type or namespace name 'vector2' could not be found (are you missing a using directive or an assembly reference?)
    
    * Doesnt complie
    
    I don't get what the error is.
    
    * implement transitions
    
    Co-authored-by: Garfield123 <michael.kulakovsky@gmail.com>

--- Assets/Scripts/CharacterControl/Character.cs
+++ Assets/Scripts/CharacterControl/Character.cs
@@ -6,16 +6,30 @@ namespace CatGame.CharacterControl
     [RequireComponent(typeof(CharacterMovement))]
     public class Character : MonoBehaviour
     {
-        public CharacterMovementConfig humanMovement = new();
-        public CharacterMovementConfig catMovement = new();
+        public float transformDelay;
 
+        [Header("Human Settings")]
+        public CharacterMovementConfig humanMovement = new();
+        public Sprite humanSprite;
+        public Vector2 humanColliderSize;
+
+        [Header("Cat Settings")]
+        public CharacterMovementConfig catMovement = new();
+        public Sprite catSprite;
+        public Vector2 catColliderSize;
+        
+        private CharacterMode _mode = CharacterMode.Human;
+        private float lastTransformTime;
+
+        private SpriteRenderer _renderer;
+        private BoxCollider2D _collider;
         private CharacterMovement _movement;
         private InteractableHandler _interactableHandler;
 
-        private CharacterMode _mode = CharacterMode.Human;
-
         private void Start()
         {
+            _renderer = GetComponent<SpriteRenderer>();
+            _collider = GetComponent<BoxCollider2D>();
             _movement = GetComponent<CharacterMovement>();
             _interactableHandler = GetComponentInChildren<InteractableHandler>();
         }
@@ -30,19 +44,67 @@ namespace CatGame.CharacterControl
 
         private void ToggleMode()
         {
+            if (Time.time - lastTransformTime <= transformDelay)
+            {
+                return;
+            }
+
+            float spriteSizeDiff = catSprite.bounds.extents.y - humanSprite.bounds.extents.y;
             if (_mode == CharacterMode.Human)
             {
+                // Check if new collider will hit anything
+                if (Physics2D.BoxCast(
+                    transform.position + new Vector3(0, spriteSizeDiff + 0.0125f),
+                    catColliderSize - new Vector2(0.025f, 0.0125f), 
+                    0, Vector2.up, 0, 1).collider != null)
+                {
+                    return;
+                }
+
                 _mode = CharacterMode.Cat;
                 _movement.SetConfig(catMovement);
                 _interactableHandler.DropPickup();
                 _interactableHandler.enabled = false;
+                _renderer.sprite = catSprite;
+                _collider.size = catColliderSize;
+                transform.position += new Vector3(0, spriteSizeDiff);
             }
             else
             {
+                // Check if new collider will hit anything
+                if (Physics2D.BoxCast(
+                    transform.position + new Vector3(0, -spriteSizeDiff + 0.0125f),
+                    humanColliderSize - new Vector2(0.025f, 0.0125f),
+                    0, Vector2.up, 0, 1).collider != null)
+                {
+                    return;
+                }
+
                 _mode = CharacterMode.Human;
                 _movement.SetConfig(humanMovement);
                 _interactableHandler.enabled = true;
+                _renderer.sprite = humanSprite;
+                transform.position += new Vector3(0, -spriteSizeDiff);
+                _collider.size = humanColliderSize;
             }
+
+            lastTransformTime = Time.time;
+        }
+        private void OnDrawGizmos()
+        {
+            float spriteSizeDiff = catSprite.bounds.extents.y - humanSprite.bounds.extents.y;
+
+            if (_mode == CharacterMode.Human)
+            {    
+                Gizmos.DrawCube(transform.position + new Vector3(0, spriteSizeDiff + 0.0125f),
+                    catColliderSize - new Vector2(0.025f, 0.0125f));
+            }
+            else
+            {
+                Gizmos.DrawCube(transform.position + new Vector3(0, -spriteSizeDiff + 0.0125f),
+                    humanColliderSize - new Vector2(0.025f, 0.0125f));
+            }
+            
         }
     }
 }
