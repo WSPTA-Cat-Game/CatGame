eb7d86510518f34abf5d810c4db8f0de5468f3ee
loukylor <loukylor@gmail.com>
2023-01-23 22:47:19 -0800

    change id tile so that it can support 2 wide tiles

--- Assets/Scripts/RuleTiles/IDTile.cs
+++ Assets/Scripts/RuleTiles/IDTile.cs
@@ -18,6 +18,8 @@ public class IDTile : RuleTile<IDTile.Neighbor>
         {
             Neighbor.SameID => tileAsID != null && tileAsID.id == id,
             Neighbor.DiffID => tileAsID != null && tileAsID.id != id,
+            Neighbor.IDTile => tileAsID != null,
+            Neighbor.Null => tileAsID == null,
             _ => base.RuleMatch(neighbor, tile),
         };
     }
@@ -26,5 +28,7 @@ public class IDTile : RuleTile<IDTile.Neighbor>
     {
         public const int SameID = 3;
         public const int DiffID = 4;
+        public const int IDTile = 5;
+        public const int Null = 6;
     }
 }
