3cbd0b16b16f918e080f1b22e443aeb8bd9780cc
loukylor <loukylor@gmail.com>
2023-02-13 19:48:48 -0800

    fix tile gameobjects being copied to 0,0,0,

---
+++ Assets/Scripts/RuleTiles/RuleTileFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.RuleTiles
+{
+    // For some reason, when built, rule tiles with a gameobject set will have
+    // their gameobject duplicated at 0, 0, 0, so let's just delete any 
+    // gameobject at 0, 0, 0
+    // https://issuetracker.unity3d.com/issues/instantiating-tilemap-that-has-rule-tile-with-default-gameobject-set-creates-extra-copy-of-gameobject-at-00-0-in-builds
+    public class RuleTileFix : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+}
