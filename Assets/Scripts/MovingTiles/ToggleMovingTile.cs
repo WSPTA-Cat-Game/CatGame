f351aa52cb316b9ce9294a087227860331631990
loukylor <loukylor@gmail.com>
2023-02-15 20:31:24 -0800

    fix tentacle shadow sticking through walls

--- Assets/Scripts/MovingTiles/ToggleMovingTile.cs
+++ Assets/Scripts/MovingTiles/ToggleMovingTile.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace CatGame.MovingTiles
 {
@@ -8,6 +9,7 @@ namespace CatGame.MovingTiles
         private SpriteMask _spriteMask;
         private SpriteRenderer _renderer;
         private BoxCollider2D _collider;
+        private ShadowCaster2D _caster;
 
         public void Toggle()
         {
@@ -23,58 +25,68 @@ namespace CatGame.MovingTiles
         protected override void Update()
         {
             // Make size of collider same size as masked in area
-            if (_spriteMask != null && _renderer != null && _collider != null)
+            if (_spriteMask == null || _renderer == null || _collider == null)
             {
-                if (!_spriteMask.bounds.Intersects(_renderer.bounds))
-                {
-                    _collider.size = Vector2.zero;
-                    return;
-                }
-
-                Bounds overlap = new();
-
-                // This assumes 2d movement
-                // This basically finds the overlap between the mask and
-                // renderer, then adjusts the size of the collider to match
-                // Essentially prevents the collider from sticking out of walls
-                float x1 = Mathf.Max(_spriteMask.bounds.min.x, _renderer.bounds.min.x);
-                float x2 = Mathf.Min(_spriteMask.bounds.max.x, _renderer.bounds.max.x);
-
-                float y1 = Mathf.Max(_spriteMask.bounds.min.y, _renderer.bounds.min.y);
-                float y2 = Mathf.Min(_spriteMask.bounds.max.y, _renderer.bounds.max.y);
-
-                float minX;
-                float maxX;
-                if (x1 < x2)
-                {
-                    minX = x1;
-                    maxX = x2;
-                }
-                else
-                {
-                    minX = x2;
-                    maxX = x1;
-                }
-
-                if (y1 < y2)
-                {
-                    overlap.min = new Vector3(minX, y1);
-                    overlap.max = new Vector3(maxX, y2);
-                }
-                else
-                {
-                    overlap.min = new Vector3(minX, y2);
-                    overlap.max = new Vector3(maxX, y1);
-                }
-
-                // Convert bounds to local coords
-                Vector3 localSize = _collider.transform.InverseTransformVector(overlap.size);
-                localSize.Set(Mathf.Abs(localSize.x), Mathf.Abs(localSize.y), Mathf.Abs(localSize.z));
-                localSize -= new Vector3(_collider.edgeRadius * 2, _collider.edgeRadius * 2);
-
-                _collider.offset = _collider.transform.InverseTransformPoint(overlap.center);
-                _collider.size = localSize;
+                return;
             }
+
+            if (!_spriteMask.bounds.Intersects(_renderer.bounds))
+            {
+                _collider.size = Vector2.zero;
+                return;
+            }
+
+            Bounds overlap = new();
+
+            // This assumes 2d movement
+            // This basically finds the overlap between the mask and
+            // renderer, then adjusts the size of the collider to match
+            // Essentially prevents the collider from sticking out of walls
+            float x1 = Mathf.Max(_spriteMask.bounds.min.x, _renderer.bounds.min.x);
+            float x2 = Mathf.Min(_spriteMask.bounds.max.x, _renderer.bounds.max.x);
+
+            float y1 = Mathf.Max(_spriteMask.bounds.min.y, _renderer.bounds.min.y);
+            float y2 = Mathf.Min(_spriteMask.bounds.max.y, _renderer.bounds.max.y);
+
+            float minX;
+            float maxX;
+            if (x1 < x2)
+            {
+                minX = x1;
+                maxX = x2;
+            }
+            else
+            {
+                minX = x2;
+                maxX = x1;
+            }
+
+            if (y1 < y2)
+            {
+                overlap.min = new Vector3(minX, y1);
+                overlap.max = new Vector3(maxX, y2);
+            }
+            else
+            {
+                overlap.min = new Vector3(minX, y2);
+                overlap.max = new Vector3(maxX, y1);
+            }
+
+            // Convert bounds to local coords
+            Vector3 localSize = _collider.transform.InverseTransformVector(overlap.size);
+            localSize.Set(Mathf.Abs(localSize.x), Mathf.Abs(localSize.y), Mathf.Abs(localSize.z));
+
+            Bounds localBounds = new(
+                _collider.transform.InverseTransformPoint(overlap.center),
+                localSize);
+
+            _caster.shapePath[0] = localBounds.min;
+            _caster.shapePath[1] = new Vector3(localBounds.min.x, localBounds.max.y);
+            _caster.shapePath[2] = localBounds.max;
+            _caster.shapePath[3] = new Vector3(localBounds.max.x, localBounds.min.y);
+
+            _collider.offset = localBounds.center;
+            _collider.size = localBounds.size - new Vector3(_collider.edgeRadius * 2, _collider.edgeRadius * 2);
         }
 
         private void Awake()
@@ -86,6 +98,7 @@ namespace CatGame.MovingTiles
 
             _renderer = GetComponent<SpriteRenderer>();
             _collider = GetComponent<BoxCollider2D>();
+            _caster = GetComponent<ShadowCaster2D>();
 
             stopTime = 0;
         }
